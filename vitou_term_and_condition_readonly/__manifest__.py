041ef59fca0ecb1228cb8adf7fa7648c583f7763
reamvitou <reamvitou@yahoo.com>
2025-04-09 20:51:19 +0700

    up

---
+++ vitou_term_and_condition_readonly/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2024-TODAY,
+#    Author: REAM Vitou (reamvitou@yahoo.com)
+#    Tel: +855 17 82 66 82
+
+
+################################################################################
+{
+    'name': 'Terms and Condition Edit Protection',
+    'version': '18.0.1.1.3',
+    'category': 'Sale',
+    'summary': 'This module, set readonly for terms and condition in sale',
+    'description': 'This module, set readonly for terms and condition in sale',
+    'author': 'V Technologies',
+    'company': 'V Technologies',
+    'maintainer': 'V Technologies',
+    'website': 'https://www.odoocambodia.com',
+    # 'price':'30.0',
+    # 'currency':'USD',
+    'depends': ['sale'],
+    'data': [
+
+             'views/term_condition_readonly.xml'
+
+             ],
+    'images': ['static/description/banner.png'],
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ vitou_term_and_condition_readonly/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <vitou_barcode_sale>
+
+#### 01.10.2024
+#### Version 18.0.1.0.0
+##### ADD
+- Initial Commit for Barcode Scanning Support For Sale and Purchase
+
